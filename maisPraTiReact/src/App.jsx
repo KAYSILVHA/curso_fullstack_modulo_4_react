b3eb6de4cc7eee606cc5495964737bdcdc4f63dd
KAYSILVHA <kaysilvha@gmail.com>
2024-07-20 17:00:14 -0300

    iniciando projeto header

---
+++ maisPraTiReact/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./App.module.css"
+import Header from './Components/Header/Header';
+
+function App() {
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App;
---
+++ maisPraTiReact/src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import "./assets/css/Header.module.css"
+
+function Header() {
+
+  return (
+    <header>
+
+    </header>
+  )
+}
+
+export default Header;
